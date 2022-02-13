95ac7f8d8dc56769b87c7ed0f41a74efdc462b31
misba0770 <80534061+misba0770@users.noreply.github.com>
2022-02-14 00:03:26 +0530

    3Sum-Unique Triplets-(O(n*2))

---
+++ targetSumUniqueTriplets.java
@@ -0,0 +1,65 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author misba zaidi
+ */
+public class targetSumUniqueTriplets {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner s=new Scanner(System.in);
+         int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=s.nextInt();
+        }
+        int target=s.nextInt();
+        Arrays.sort(arr);
+       threeSum(arr,n,target);
+    }
+
+    private static void threeSum(int[] arr, int n,int target) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for(int i=0;i<n-2;i++){
+            if(i!=0&&arr[i]==arr[i-1])continue;
+            int val1=arr[i];
+            int tar=target-val1;
+             List<List<Integer>> x=twoSum(arr,i+1,n-1,tar);
+             for(List<Integer> subRes:x){subRes.add(arr[i]);Collections.sort(subRes);System.out.println(subRes);}
+        }
+    }
+
+    private static List<List<Integer>> twoSum(int[] arr, int i, int j,int target) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<List<Integer>> ans=new ArrayList<>();
+        while(i<j){
+            if(i!=0&&arr[i]==arr[i-1]){i++;continue;}
+            int sum=arr[i]+arr[j];
+            if(sum==target){
+                List<Integer> subList=new ArrayList<>();
+                subList.add(arr[i]);subList.add(arr[j]);
+                ans.add(subList);
+                i++;
+                j--;
+            }else if(sum>target)j--;
+            else i++;
+        }
+        return ans;
+    }
+    
+}
