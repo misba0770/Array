7f60b598d20427b70c2ab0b71f5bf677f7425138
misba0770 <80534061+misba0770@users.noreply.github.com>
2022-04-23 16:42:46 +0530

    Create plusOne.java

---
+++ plusOne.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int[] plusOne(int[] A) {
+         int carry=1,p=0;
+        for(int i=A.length-1;i>=p;i--)
+        {
+            int sum=A[i]+carry;
+            A[i]=sum%10;
+            carry=sum/10;
+        }
+        if(carry>0)
+        {
+            int b[]=new int[A.length+1];
+            b[0]=carry;
+            for(int i=1;i<A.length+1;i++)
+            b[i]=A[i-1];
+            return b;
+        }
+        else
+        {
+          int pp=0;
+          while(A[pp]==0)
+          pp++;
+            int b[]=new int[A.length-pp];
+            for(int i=pp;i<A.length;i++)
+            b[i-pp]=A[i];
+            return b;
+        }
+    }
+}
